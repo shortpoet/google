7e435561232732b0c59e7552b53bb3250b8b574b
shortpoet <soriano.carlitos@gmail.com>
2020-03-30 15:48:51 -0500

    config file works

--- google-api/unsubscribe/models/sqlClient.js
+++ google-api/unsubscribe/models/sqlClient.js
@@ -3,16 +3,15 @@
 const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const KEY_PATH = '../../config/sqlClient.config.json';
-const keyPath = path.join(__dirname, KEY_PATH);
-
-const keyFile = require(keyPath);
+const CONFIG_PATH = '../../config/sqlClient.config.json';
+const configPath = path.join(__dirname, CONFIG_PATH);
+const config = require(configPath);
 
 
 class SqlClient {
   constructor(options) {
     var sample = 'mssql://user:pass@example.com:5432/dbname'
-    this.sql = new Sequelize('Google', 'node_client', 'dFicJotG5hHPTCY1flfq', {
+    this.sql = new Sequelize(config.database, config.username, config.password, {
       host: 'localhost',
       dialect: 'mssql',
     //   pool: {
