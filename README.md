98d10933a5408ca428786983ef80e44cbe84f1a0
shortpoet <soriano.carlitos@gmail.com>
2020-04-11 14:41:04 -0500

    basic vue app added

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # google
 
 https://apiharmony-open.mybluemix.net/public/apis/google_sheets#post_v4_spreadsheets
+
+## Dashboard
+
+dotnet new webapp --auth IndividualB2C
+
+Creating this template will make changes to existing files:
+  Overwrite   appsettings.Development.json
+  Overwrite   appsettings.json
+  Overwrite   Dashboard.csproj
+html
+  Overwrite   Properties/launchSettings.json
+
+Rerun the command and pass --force to accept and create.
+
+dotnet new react --force
+
+
+added to .csproj:
+
+```c#
+<UserSecretsId>aspnet-Dashboard-7D5A78D3-C561-4B7E-9638-4CC29396CF44</UserSecretsId>
+```
+
+added to startup:
+
+- imports
+
+```c#
+services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+    .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+services.AddRazorPages();
+```
+
+added to appsetting:
+
+- adB2C config
+
+```
+dotnet add package Microsoft.AspNetCore.Authentication.AzureADB2C.UI
+dotnet add package VueCliMiddleware
+```
---
+++ google-api/Dashboard/VueClient/vue.config.js
@@ -0,0 +1,50 @@
+// const fs = require('fs')
+// const path = require('path')
+
+module.exports = {
+  css: {
+    // fixed css loader issue for npm from bash 
+    extract: false,
+    // loaderOptions: {
+    //   scss: {
+    //     prependData: `@import "~@/assets/scss/_variables.scss";`
+    //   }
+    // }
+  },
+  configureWebpack: {
+    devtool: 'source-map',
+    // plugins: [
+    //   new GoogleFontsPlugin('./src/assets/fonts.json')
+    // ],  
+    },
+    // baseUrl: process.env.NODE_ENV === 'production' ? '/subpath/' : '/',
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/production-sub-path/'
+    //   : '/',
+  devServer: {
+    // proxy: 'https://localhost:8080',
+    // adding public seemed to do the same as
+    // adding the below cmd to package.json
+    // "servelocal": "vue-cli-service serve --host localhost",
+    // public: 'https://localhost:8080/',
+    // port: 8080, // CHANGE YOUR PORT HERE!
+    https: false,
+    // hotOnly: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+    // http://vuejs-templates.github.io/webpack/static.html
+    // https://stackoverflow.com/questions/43879418/deploy-vuejs-app-in-a-subdirectory/43880261#43880261
+    // https://cli.vuejs.org/guide/html-and-static-assets.html
+    // Other options include:
+    // outputDir: undefined,
+    // assetsDir: undefined,
+    // runtimeCompiler: undefined,
+    // productionSourceMap: undefined,
+    // parallel: undefined,
+    // css: undefined
+  }
+}
+  
