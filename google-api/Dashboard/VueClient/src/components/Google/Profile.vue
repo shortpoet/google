54e4562a3fd3f845f03200af0cc46761608a7aeb
shortpoet <soriano.carlitos@gmail.com>
2020-04-17 12:37:32 -0500

    suddenly the messages and labes stopped working again with no apparent changes to that part of code

--- google-api/Dashboard/VueClient/src/components/Google/Profile.vue
+++ google-api/Dashboard/VueClient/src/components/Google/Profile.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="item-container" v-if="items">
-    <pre v-html="items"></pre>
+  <div class="profile-container">
+    
   </div>
 </template>
 
@@ -23,48 +23,8 @@ export default {
   },
   methods: {
     ...mapActions('google', ['initClient']),
-    getData(gapiClient) {
-      let vm = this;
-      console.log('getting gmail label data')
-      console.log(gapiClient)
-      console.log(vm.getGapiClient)
-
-      gapiClient.client.gmail.users.labels.list({
-        'userId': 'me'
-      })     
-      .then(response => {
-        console.log(response)
-        vm.items = this.syntaxHighlight(response.result.labels);
-        console.log(vm.items);
-      });
-    },
-    syntaxHighlight(json) {
-      if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
-      }
-      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-      return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, match => {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-          if (/:$/.test(match)) {
-            cls = 'key';
-          } else {
-            cls = 'string';
-          }
-        } else if (/true|false/.test(match)) {
-          cls = 'boolean';
-        } else if (/null/.test(match)) {
-          cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-      });
-    }
   },
   mounted() {
-    this.initClient({scope: 'gmail', discoveryDocs: 'gmail'}).then((gapiClient) => {
-      console.log('going to get data')
-      this.getData(gapiClient);
-    })
   }
 }
 </script>
